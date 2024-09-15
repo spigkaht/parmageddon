44938119932bed8a5f3519e527fba9af368848e2
Benjamin Jackson <benj@betterrentals.com.au>
2024-09-15 19:54:08 +1000

    models & migrations created

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_15_095310) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "comment"
+    t.bigint "venue_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.decimal "total_rating", precision: 2, scale: 1
+    t.decimal "chicken_rating", precision: 2, scale: 1
+    t.decimal "crumb_rating", precision: 2, scale: 1
+    t.decimal "topping_rating", precision: 2, scale: 1
+    t.decimal "sides_rating", precision: 2, scale: 1
+    t.decimal "venue_rating", precision: 2, scale: 1
+    t.decimal "price", precision: 4, scale: 2
+    t.index ["venue_id"], name: "index_reviews_on_venue_id"
+  end
+
+  create_table "venues", force: :cascade do |t|
+    t.string "name"
+    t.string "street"
+    t.string "city"
+    t.string "state"
+    t.string "postcode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.decimal "latitude", precision: 10, scale: 6
+    t.decimal "longitude", precision: 10, scale: 6
+    t.decimal "total_rating_average", precision: 2, scale: 1
+    t.decimal "chicken_rating_average", precision: 2, scale: 1
+    t.decimal "crumb_rating_average", precision: 2, scale: 1
+    t.decimal "topping_rating_average", precision: 2, scale: 1
+    t.decimal "sides_rating_average", precision: 2, scale: 1
+    t.decimal "venue_rating_average", precision: 2, scale: 1
+    t.decimal "price_average", precision: 4, scale: 2
+  end
+
+  add_foreign_key "reviews", "venues"
 end
