932525ff563b0a1a463e697debdf98c426a8b096
Benjamin Jackson <benj@betterrentals.com.au>
2024-11-03 09:55:05 +1100

    added socials links

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Parmageddon
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
+    config.middleware.use Rack::Deflater
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
